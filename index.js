956d6bc79080f2bf66813596a5ce85cb7f8bfa6e
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-10 10:27:08 -0800

    Create spacing in between each word going into the html.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let sentence = ["Hello ", "my ", "name ", "is ", "Per"] 
+let sentence = ["Hello", "my", "name", "is", "Per"] 
 let greetingEl = document.getElementById("greeting-el")
 
 // Render the sentence in the greetingEl paragraph using a for loop and .textContent
-
+// How do you keep the spaces between the words if I remve them from the array?
 for (i = 0; i < sentence.length; i ++) {
-  greetingEl.textContent += sentence[i]
+  greetingEl.textContent += sentence[i] + " "
 }
 console.log(greetingEl.textContent)
