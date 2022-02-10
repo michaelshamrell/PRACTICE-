7719d8cd61ceb4848043d556880dac3c3d95443a
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-10 10:12:57 -0800

    Create a loop that logs out all the cards in the array, use cards.length to specify how long the loop should run for.

--- index.js
+++ index.js
@@ -121,5 +121,12 @@ let messages = [
   "Hey, how's it going?",        
   "I'm great, thank you! How about you?",
   "All good. Been working on my portfolio lately.",
-  "Same here!"
+  "Same here!",
+  "great to hear",
+  "SUPER"
 ]
+
+for (let i = 0; i < messages.length; i += 1) {
+  console.log(messages[i])
+}
+
