ffe6944a4a76b46e6681ada3fe2c0bb0d5c43dd5
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-16 12:48:03 -0800

    Follow prompt that says the create a function that uses a person bject to create a formatted string, then have it console.log to show its working.

--- index.js
+++ index.js
@@ -290,3 +290,25 @@ let castle = {
 console.log(castle.location)
 console.log(castle.amenities)
 console.log(castle.price)
+
+//PRACTICE=-----------------------------------
+// Create a person object that contains three keys: name, age, and county.
+// Use yourself as an example to set the values for name, age, and country
+
+// Create a function, logData(), that uses the person object to create a
+// string in the following format: 
+// "Per is 35 years old and lives in Norway"
+
+// Call the logData() function to verify that it works
+
+let person = {
+  name: "Michael Shamrell",
+  age: 20,
+  country: "USA"
+}
+
+function logData() {
+  console.log( person.name + " is " + person.age + " and lives in " + person.country )
+}
+
+logData()
