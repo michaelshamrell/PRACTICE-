61c4050afbae7bd4b8969835aaff89821820c83f
michaelshamrell <michaelshamrell2001@gmail.com>
2022-03-09 23:37:21 -0800

    finish fruit looping practice

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ let orangeShelf = document.getElementById("orange-shelf")
 // and the oranges onto the orangeShelf. Use a for loop,
 // a conditional statement, and the textContent property.
 
-let appleShelf = 
+function sortFruit() {
+  for (let i = 0; i < fruit.length; i++) {
+      if (fruit[i] === "🍎") {
+          appleShelf.textContent += "🍎"
+      } else if (fruit[i] === "🍊") {
+          orangeShelf.textContent += "🍊"
+      }
+  }
+}
+
+
+sortFruit();
